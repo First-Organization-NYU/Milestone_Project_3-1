1d50f0db72b77c7216401c50568d5c36e2e18bd2
Amy Jiang <Amii>
2023-04-27 15:05:09 -0400

    testing

--- my-app/servers/controllers/cart_controller.js
+++ my-app/servers/controllers/cart_controller.js
@@ -1,2 +1,44 @@
-const cart = require('express').Router()
+const cartItems = require('express').Router()
 const db = require('../../models')
+const {Cart} = db
+const {Op} = require("sequelize")
+
+// Get All Cart Items
+cartItems.get('/', async(req, res) => {
+    try {
+        const foundCartItems = await Cart.finadAll(
+            {
+                where: {
+                    name: {[Op.like]: `%${req.query.name ? req.query.name: ''}%`}
+                }
+            }
+        )
+        res.status(200).json(foundCartItems)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// Get a Cart Item
+cartItems.get('/barcode', async(req,res) => {
+    try{
+        const foundCartItem = await Cart.findOne(
+            {
+                where: {dog_barcode: req.params.barcode}
+            })
+            console.log(foundCartItem.dataValues)
+            res.status(200).json(foundCartItem.dataValues)
+    }catch(error){
+        res.status(500).json(error)
+    }
+})
+
+// Create a Cart Item
+
+
+
+// Edit A Cart Item
+
+
+
+// Delete a Cart Item - changing 
