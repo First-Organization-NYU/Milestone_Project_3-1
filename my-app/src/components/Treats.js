86d7e4a144fc93ced3a6d414414e18c5741c22d6
kamel Barakat <kamelbarakat96@yahoo.com>
2023-05-01 19:34:55 -0400

    treats and toys page update

--- my-app/src/components/Treats.js
+++ my-app/src/components/Treats.js
@@ -1,35 +1,36 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function Treats() {
   //fetch data from dog_treats table & display it 
-  const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("http://localhost:3002/toys");
+      const result = await axios("http://localhost:3002/treats");
       setData(result.data)
     }
     fetchData()
-  },[])
+  }, [])
 
   return (
     <div className="treatsPage">
       <h1>Shop For Dog Treats</h1>
-      <br></br>
-      {data.map((dog_treats) => (
-        <div className="productDisplay" key={dog_treats.barcode}>
-          <h3>{dog_treats.name}</h3>
-          <h4>{dog_treats.brand}</h4>
-          <img src={dog_treats.image} alt="Dog Treats"></img>
-          <h5>{dog_treats.price}</h5>
-          <h6>{dog_treats.weight}</h6>
-          <button className="add-btn">Add</button>
-          <br></br>
-        </div>
-      ))}
+      <div className="treatContent">
+        {data.map((dog_treats) => (
+          <div className="treatDisplay" key={dog_treats.barcode}>
+            <h3><u>{dog_treats.name}</u></h3>
+            <h4>{dog_treats.brand}</h4>
+            <img src={dog_treats.image} alt="Dog Treats"></img>
+            <h5>Price: ${dog_treats.price}</h5>
+            <h6>Product Weight: {dog_treats.weight}</h6>
+            <button className="add-btn">Add</button>
+            <br></br>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
